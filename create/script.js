b236116799834bd90254a86c3c26565a6602949a
LeclercqL <104424012+LeclercqL@users.noreply.github.com>
2022-06-03 10:11:57 +0200

    New version of exo "intro"

--- create/script.js
+++ create/script.js
@@ -1,7 +1,8 @@
+addElement()
 function addElement () {
     let newSection = document.createElement('section');
     let apprenant1 = document.createTextNode('Latifa');
-    newSection.appendChild(Latifa);
+    newSection.appendChild(apprenant1);
     let article = document.getElementById("p");
     document.body.insertBefore(newSection, article);
 }
--- intro/script.js
+++ intro/script.js
@@ -1,6 +1,17 @@
-document.title = "Modifying the DOM";
 console.log(document.title);
+document.title = "Modifiyng the DOM";
 document.body.style.backgroundColor = "#FF69B4";
-for(const element of document.body.getElementsByTagName("*")){
-    console.log(element)
-}
+
+function randomBgColor() { // https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
+    var x = Math.floor(Math.random() * 255);
+    var y = Math.floor(Math.random() * 255);
+    var z = Math.floor(Math.random() * 255);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    document.body.style.background = bgColor;
+    }
+randomBgColor();
+
+for(const element of document.body.children)
+console.log(document.body.children);
+
+randomBgColor()
